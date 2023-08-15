a951df5632addba35074a22a9460ed1d9cca6d77
Raman Shalupau <raman.shalupau@gmail.com>
2023-08-15 14:59:13 +0800

    UI improvements in the admin dashboard

--- plugins/backlink-cafe/src/admin/components/Offer.js
+++ plugins/backlink-cafe/src/admin/components/Offer.js
@@ -46,6 +46,8 @@ export function Offer( offer ) {
 	};
 
 	const reject = async () => {
+		const confirmed = confirm( 'Are you sure you want to reject this offer?' );
+		if (!confirmed) return
 		await rejectOffer( {
 			data: {
 				offerId: id,
@@ -58,45 +60,45 @@ export function Offer( offer ) {
 
 	return (
 		<li>
-			<div className="flex flex-col gap-2 p-2 px-4 pb-4 -ml-4 hover:bg-gray-200 rounded-xl">
+			<div className="flex flex-col gap-2 p-2 px-4 pb-4 -ml-4 hover:bg-gray-200 rounded-xl text-[10px]">
 				<div>
-					<h2 title="Target URL" className="text-lg">
-						<span className="opacity-50">Link to</span>{ ' ' }
-						<b>{ order.url }</b>
-					</h2>
+					<p title="Target URL" className="text-lg">
+						<span className="opacity-80">Link to</span>{ ' ' }
+						<a href={ order.url } target="_blank" className="font-bold underline" rel='noopener' >{ order.url }</a>
+					</p>
 					<p title="Anchor text" className="text-lg">
-						<span className="opacity-50">with anchor</span>{ ' ' }
-						<b>{ order.anchor }</b>
+						<span className="opacity-80">with anchor text</span>{ ' ' }
+						"<b>{ order.anchor }</b>"
 					</p>
 					{ order.priceBTC && (
 						<p title="Price in BTC" className="text-lg">
-							<span className="opacity-50">price in BTC</span>{ ' ' }
+							<span className="opacity-80">price in BTC</span>{ ' ' }
 							<b>{ order.priceBTC }</b>
 						</p>
 					) }
 
 					{ /* TODO we need to show the exact anchor that actually got a match. Not the whole anchor value (often it's comma separated) */ }
 					<p title="Page URL on your site" className="text-lg">
-						<span className="opacity-50">to be placed on</span>{ ' ' }
-						<b>{ offer.url }</b>
+						<span className="opacity-80">will be placed on your page</span>{ ' ' }
+						<a href={ offer.url } target="_blank" className="underline" >{ offer.url }</a>
 					</p>
-					{ /* <p className="text-lg"><code>&lt;a href="{ order.url }"&gt;{ order.anchor }&lt;/a&gt; </code></p> */ }
+					{ /* <p className=""><code>&lt;a href="{ order.url }"&gt;{ order.anchor }&lt;/a&gt; </code></p> */ }
 				</div>
 
 				{ status === 'PENDING' && (
-					<div className="flex gap-2">
+					<div className="flex gap-2 text-lg">
 						<button
-							className="px-6 py-2 text-lg font-bold text-white duration-200 bg-black rounded hover:shadow-lg hover:scale-105 "
+							className="p-1 px-4 text-white bg-black rounded hover:opacity-80"
 							onClick={ accept }
 						>
-							Accept
+							Accept for ${offer.price/100}
 						</button>
 						<button
-							className="px-6 py-2 text-lg font-bold text-red-400 duration-200 rounded hover:bg-red-400 hover:text-white"
+							className="p-1 px-4 rounded opacity-80 hover:bg-gray-300 hover:opacity-100"
 							title="Skip this offer"
 							onClick={ reject }
 						>
-							Skip
+							Reject
 						</button>
 					</div>
 				) }
@@ -123,6 +125,14 @@ export function Offer( offer ) {
 							Paid
 						</div>
 					</div>
+				) }
+
+				{ status === 'REJECTED' && (
+					<div className="flex gap-2 text-lg">
+						<span className="p-1 px-4 border border-gray-500 border-solid rounded opacity-80">
+							You rejected this offer
+						</span>
+				</div>
 				) }
 			</div>
 		</li>
